84ddf825418eb059d3a32375d50fccf20ef78207
Bartek Duczmal <87827238+BartoszDuczmal@users.noreply.github.com>
2024-09-19 17:28:26 +0200

    Add files via upload

---
+++ main (1).cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+//Definicje
+float bmi = 0;
+bool gender();
+float weight();
+float height();
+
+int main() {
+    cout << "Zad+ -> Program do obliczania BMI" << endl;
+    gender();
+    bmi = weight() / (pow(2, (height() / 100 )));
+    cout << "Twoje BMI wynosi " << bmi;
+}
+bool gender()
+{
+    char temp;
+    cout << "Podaj swoja płeć ( M lub K ): ";
+    cin >> temp;
+    return ( temp == 'M' || temp == 'm' ) ? true : false;
+}
+float weight()
+{
+    float temp;
+    cout << "Podaj swoją wagę ( XX.X ): ";
+    cin >> temp;
+    return temp;
+}
+float height()
+{
+    float temp;
+    cout << "Podaj swój wzrost ( w centymetrach ): ";
+    cin >> temp;
+    return temp;
+}
