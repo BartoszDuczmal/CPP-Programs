fbf0323cf6d09b53a89c97fadf1f4a4cbc39e20c
Bartek Duczmal <87827238+BartoszDuczmal@users.noreply.github.com>
2024-10-03 08:07:44 +0200

    Update L22_2g1_BarDuc_nwd(1).cpp

--- L22_2g1_BarDuc_nwd(1).cpp
+++ L22_2g1_BarDuc_nwd(1).cpp
@@ -5,13 +5,10 @@ using namespace std;
 int main() {
     int a, b;
 
-    // Kr01: Pobranie liczb a, b
     cout << "Podaj dwie liczby: ";
     cin >> a >> b;
 
-    // Kr02: Dopóki a != b, wykonuj krok Kr03
     while (a != b) {
-        // Kr03: Jeśli a < b, to b <- b - a, w przeciwnym razie a <- a - b
         if (a < b) {
             b = b - a;
         } else {
@@ -19,9 +16,7 @@ int main() {
         }
     }
 
-    // Kr04: Wypisz zawartość zmiennej a (lub b) - to jest NWD
     cout << "NWD: " << a << endl;
 
-    // Kr05: KONIEC
     return 0;
 }
